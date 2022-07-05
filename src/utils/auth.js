d9c0a71ea221ed128e4beec6938710765ea5aae8
Julia Rulova <julia.rulova0066@gmail.com>
2022-07-05 13:56:18 +0300

    fix auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -36,11 +36,12 @@ export const authorize = (email, password) => {
         .then(checkResOk);
 };
 
-export const checkToken = () => {
+export const checkToken = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
         credentials: 'include'
     })
